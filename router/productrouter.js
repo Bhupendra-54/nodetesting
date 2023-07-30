55f440cf16d5a5672ac3d835e99202ef7fa65f40
Bhupendra <bschauhangr8@gmail,com>
2023-07-30 11:36:46 +0530

    first commit

--- router/productrouter.js
+++ router/productrouter.js
@@ -11,3 +11,12 @@ Router
     .post('/auc', productsRouter.postadd)
     .post('/products/:id', productsRouter.getone)
 exports.router=Router
+
+
+// echo "# nodetesting" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Bhupendra-54/nodetesting.git
+// git push -u origin main
